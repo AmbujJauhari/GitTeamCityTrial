cbef30cfbc69e0d248f671262726175603f66f05
Ambuj Jauhari <ambujjauhari@yahoo.co.in>
2016-03-10 23:55:35 +0530

    Deleting groovy file and adding java file

---
+++ src/test/java/com/ambuj/tc/TCTest.java
@@ -0,0 +1,13 @@
+package com.ambuj.tc;
+
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class TCTest {
+
+    @Test
+    public void testTeamCity() {
+        TestCase.assertTrue(1 == 1);
+    }
+}
