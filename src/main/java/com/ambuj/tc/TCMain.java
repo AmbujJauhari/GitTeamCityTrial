6909de1f8ca455fe3d2ee4e2b300a66dce52ff35
Ambuj Jauhari <ambujjauhari@yahoo.co.in>
2016-03-10 23:39:45 +0530

    Tc Test Main

---
+++ src/main/java/com/ambuj/tc/TCMain.java
@@ -0,0 +1,7 @@
+package com.ambuj.tc;
+
+public class TCMain {
+    public static void main(String[] args) {
+        System.out.println("This is a teamcity git test");
+    }
+}
---
+++ src/test/java/com/ambuj/tc/TCMainTest.groovy
@@ -0,0 +1,12 @@
+package com.ambuj.tc
+
+import junit.framework.TestCase
+import org.junit.Test
+
+class TCMainTest {
+
+    @Test
+    public void testTeamCity() {
+        TestCase.assertEquals(1 == 1);
+    }
+}
