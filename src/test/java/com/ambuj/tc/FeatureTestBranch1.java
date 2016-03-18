93b7317262237ec6849549c749f375ab872af8e4
Ambuj Jauhari <ambujjauhari@yahoo.co.in>
2016-03-18 08:09:58 +0530

    New test from feautre branch 1

---
+++ src/test/java/com/ambuj/tc/FeatureTestBranch1.java
@@ -0,0 +1,15 @@
+package com.ambuj.tc;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by Aj on 18-03-2016.
+ */
+public class FeatureTestBranch1 {
+
+    @Test
+    public void feature_test_from_branch1() {
+        TestCase.assertTrue(1 == 1);
+    }
+}
